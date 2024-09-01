03f9f343c7f401bbdaa2f63d85ce67df3e8088b8
Mario Giordano <164010608+LesPaul997@users.noreply.github.com>
2024-09-01 18:54:04 +0200

    Initial commit

---
+++ src/test/java/org/hello/spring/mvc/SpringLaMiaPizzeriaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hello.spring.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLaMiaPizzeriaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
