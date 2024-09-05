928f2f63017c06ccfb9c59a57f4ebf5fbb33a674
Mario Giordano <164010608+LesPaul997@users.noreply.github.com>
2024-09-05 08:58:15 +0200

    Pizza Controller database idex

--- src/main/java/org/hello/spring/mvc/controller/PizzaController.java
+++ src/main/java/org/hello/spring/mvc/controller/PizzaController.java
@@ -1,7 +1,13 @@
 package org.hello.spring.mvc.controller;
 
+import java.util.List;
+
+import org.hello.spring.mvc.model.Pizza;
+import org.hello.spring.mvc.repo.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,11 +16,14 @@ public class PizzaController {
 	
 	//repository field con autowired pr d.i.
 	
+	@Autowired
+	private PizzaRepository repo;
+	
+	@GetMapping()
 	public String index(Model model) {
 		
-		//prendo i dati da consegnare a pizze 
-		//li inserisco nel modello
-		
+		// consegna dei dati a pizza/index 
+		model.addAttribute("pizze", repo.findAll());
 		
 		return "/pizze/index";
 	}
--- src/main/java/org/hello/spring/mvc/repo/PizzaRepository.java
+++ src/main/java/org/hello/spring/mvc/repo/PizzaRepository.java
@@ -1,10 +1,9 @@
 package org.hello.spring.mvc.repo;
 
-import java.awt.print.Book;
-
+import org.hello.spring.mvc.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PizzaRepository extends JpaRepository<Book, Integer> {
+public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
 
 	// In automatico ho tutto il necessario, ma posso aagiungere eventuali metodi e funzionalità
 	
